1a987453c446558a1d5a8a9b4830e187864de7a1
matheusmarques <marqueslarreamatheus@gmail.com>
2026-03-09 13:26:18 -0400

    finalizei os botões e arrumei o modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,118 +1,173 @@
-
-import { useState, useEffect } from 'react'
-import {Trash2, Moon, Sun} from "lucide-react"
-import './App.css'
+import Calendar from "react-calendar";
+import { useState, useEffect } from "react";
+import { Trash2, Moon, Sun, Pencil, Save, CalendarDays } from "lucide-react";
+import "./App.css";
 
 function App() {
- const [features, setFeatures] = useState([
-  { id: 1, title: "Adicionar itens" },
-  { id: 2, title: "Alterar cores da página" },
-  { id: 3, title: "Organizar layout visualmente" }
- ])
+  const [features, setFeatures] = useState([
+    { id: 1, title: "Adicionar itens" },
+    { id: 2, title: "Alterar cores da página" },
+    { id: 3, title: "Organizar layout visualmente" },
+  ]);
 
- const [newFeature, setNewFeature] = useState("")
+  const [newFeature, setNewFeature] = useState("");
 
- function handleAddFeature() {
-  if (newFeature.trim() === "") return
-  const newItem = {
-    id: Date.now(),
-    title: newFeature
+  function handleAddFeature() {
+    if (newFeature.trim() === "") return;
+    const newItem = {
+      id: Date.now(),
+      title: newFeature,
+    };
+
+    setFeatures([...features, newItem]);
+    setNewFeature("");
   }
 
-  setFeatures([...features, newItem])
-  setNewFeature("")
- }
+  const [themeColor, setThemeColor] = useState("blue");
 
- const [themeColor, setThemeColor] = useState("blue")
+  function handleDelete(id) {
+    const filtered = features.filter((feature) => feature.id !== id);
+    setFeatures(filtered);
+  }
 
- function handleDelete(id) {
-  const filtered = features.filter(feature => feature.id !== id)
-  setFeatures(filtered)
- }
-
- function renderCard(feature) {
+  function renderCard(feature) {
     return (
-      <div key={feature.id} className='card'>
-        <button 
-        className='delete-btn'
-        onClick={() => handleDelete(feature.id)}
-        >
+      <div key={feature.id} className="card">
+        <button className="delete-btn" onClick={() => handleDelete(feature.id)}>
           <Trash2 size={18} />
         </button>
 
-        <p className='card-text'>
-          {feature.title}
-        </p>
+        <p className="card-text">{feature.title}</p>
 
-        <button className='view-btn'
-        onClick={() => handleView(feature)}
-        >
+        <button className="view-btn" onClick={() => handleView(feature)}>
           Vizualizar
         </button>
       </div>
-    )
- }
-
- const [isModalOpen, setIsModalOpen] = useState(false)
- const [selectedFeature, setSelectedFeature] = useState(null)
-
- function handleView (feature) {
-  setSelectedFeature(feature)
-  setIsModalOpen(true)
- }
-
- const [darkMode, setDarkMode] = useState(false)
-
- useEffect(() => {
-  if (darkMode) {
-    document.body.classList.add("dark-body")
-  } else {
-    document.body.classList.remove("dark-body")
+    );
   }
- }, [darkMode])
 
- return (
-  <>
-  <div className={`container ${themeColor}`}>
-    <h1>Painel de Funcionalidades</h1>
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedFeature, setSelectedFeature] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showCalendar, setShowCalendar] = useState(false)
 
-    <div className='input-area'>
-      <input type="text"
-      placeholder='Digite uma nova funcionalidade'
-      value={newFeature}
-      onChange={(e) => setNewFeature(e.target.value)} />
-      <button onClick={handleAddFeature}>Adicionar</button>
-    </div>
+  function handleView(feature) {
+    setSelectedFeature(feature);
+    setEditedText(feature.title);
+    setIsModalOpen(true);
+    setIsEditing(false);
+  }
 
-    <div className='theme-buttons'>
-      <button onClick={() => setThemeColor("blue")}>Azul</button>
-      <button onClick={() => setThemeColor("green")}>Verde</button>
-      <button onClick={() => setThemeColor("purple")}>Roxo</button>
-      <button className={`dark-toggle ${darkMode ? "dark-active" : ""}`}
-      onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? <Sun size={20}/> : <Moon size={20}/>}
-      </button>
-    </div>
+  function handleSaveEdit() {
+    const updated = features.map((feature) =>
+      feature.id === selectedFeature.id
+        ? { ...feature, title: editedText }
+        : feature,
+    );
 
-    <div className='card-container'>
-      {features.map(renderCard)}
-    </div>
-  </div>
+    setFeatures(updated);
 
-  {isModalOpen && (
-    <div className='modal-overlay'>
-      <div className='modal'>
-          <h2>Detalhes da Funcionalidade</h2>
-          <p>{selectedFeature?.title}</p>
+    setSelectedFeature({
+      ...selectedFeature,
+      title: editedText,
+    });
 
-          <button onClick={() => setIsModalOpen(false)}>
-            Fechar
+    setIsEditing(false);
+  }
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-body");
+    } else {
+      document.body.classList.remove("dark-body");
+    }
+  }, [darkMode]);
+
+  return (
+    <>
+      <div className={`container ${themeColor}`}>
+        <h1>Painel de Funcionalidades</h1>
+
+        <div className="input-area">
+          <input
+            type="text"
+            placeholder="Digite uma nova funcionalidade"
+            value={newFeature}
+            maxLength={900}
+            onChange={(e) => setNewFeature(e.target.value)}
+          />
+          <button onClick={handleAddFeature}>Adicionar</button>
+        </div>
+
+        <div className="theme-buttons">
+          <button onClick={() => setThemeColor("blue")}>Azul</button>
+          <button onClick={() => setThemeColor("green")}>Verde</button>
+          <button onClick={() => setThemeColor("purple")}>Roxo</button>
+          <button
+            className={`dark-toggle ${darkMode ? "dark-active" : ""}`}
+            onClick={() => setDarkMode(!darkMode)}
+          >
+            {darkMode ? <Sun size={20} /> : <Moon size={20} />}
           </button>
+        </div>
+
+        <div className="card-container">{features.map(renderCard)}</div>
       </div>
-    </div>
-  )}
-  </>
- )
+
+      {isModalOpen && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Detalhes da Funcionalidade</h2>
+
+            <div className="modal-text">
+              {isEditing ? (
+                <textarea
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
+                  rows={4}
+                  style={{ width: "100%" }}
+                />
+              ) : (
+                <p>{selectedFeature?.title}</p>
+              )}
+            </div>
+
+            {showCalendar && (
+             <div style={{ margin: "15px 0" }}>
+              <Calendar onChange={setDate} value={date} />
+              <p>Data selecionada: {date.toLocaleDateString()}</p>
+            </div>
+            )}
+
+            <div className="modal-buttons">
+              {!isEditing && (
+                <button onClick={() => setIsEditing(true)}>
+                  <Pencil size={16} /> Editar
+                </button>
+              )}
+
+              {isEditing && (
+                <button onClick={handleSaveEdit}>
+                  <Save size={16} />
+                  Salvar
+                </button>
+              )}
+
+              <button onClick={() => setShowCalendar(!showCalendar)}>
+                <CalendarDays size={16}/> Data
+              </button>
+
+              <button onClick={() => setIsModalOpen(false)}>Fechar</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
